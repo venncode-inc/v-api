7a876d37e2ec1c390743e5c87c568776cffe6ead
Hz4E!_ <mrayhan1924@gmail.com>
2025-06-07 00:59:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,9 @@ app.set("json spaces", 2);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use('/', express.static(path.join(__dirname, 'api-page')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 
-// static docs API di /
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-// static menu utama API di /api
-app.use('/api-page', express.static(path.join(__dirname, 'api-page')));
-
-// middleware untuk response JSON
 const settingsPath = path.join(__dirname, './src/settings.json');
 const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf-8'));
 
@@ -40,24 +35,22 @@ app.use((req, res, next) => {
     next();
 });
 
-// load route API dengan prefix /api
-const apiFolder = path.join(__dirname, './src/api');
+// Api Route
 let totalRoutes = 0;
-
+const apiFolder = path.join(__dirname, './src/api');
 fs.readdirSync(apiFolder).forEach((subfolder) => {
     const subfolderPath = path.join(apiFolder, subfolder);
     if (fs.statSync(subfolderPath).isDirectory()) {
         fs.readdirSync(subfolderPath).forEach((file) => {
+            const filePath = path.join(subfolderPath, file);
             if (path.extname(file) === '.js') {
-                const filePath = path.join(subfolderPath, file);
-                require(filePath)(app, '/api');
+                require(filePath)(app);
                 totalRoutes++;
-                console.log(chalk.bgHex('#FFFF99').hex('#333').bold(` Loaded Route: ${file} `));
+                console.log(chalk.bgHex('#FFFF99').hex('#333').bold(` Loaded Route: ${path.basename(file)} `));
             }
         });
     }
 });
-
 console.log(chalk.bgHex('#90EE90').hex('#333').bold(' Load Complete! ✓ '));
 console.log(chalk.bgHex('#90EE90').hex('#333').bold(` Total Routes Loaded: ${totalRoutes} `));
 
@@ -65,19 +58,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'api-page', 'index.html'));
 });
 
-// fallback 404
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).sendFile(process.cwd() + "/api-page/404.html");
 });
 
-// fallback 500
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).sendFile(path.join(__dirname, 'public', '500.html'));
+    res.status(500).sendFile(process.cwd() + "/api-page/500.html");
 });
 
 app.listen(PORT, () => {
-    console.log(chalk.bgHex('#90EE90').hex('#333').bold(` Server running on port ${PORT} `));
+    console.log(chalk.bgHex('#90EE90').hex('#333').bold(` Server is running on port ${PORT} `));
 });
 
 module.exports = app;
