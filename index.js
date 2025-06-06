2cbdc3d635d0b650f2217374b8f000c1ab7b4d3a
Hz4E!_ <mrayhan1924@gmail.com>
2025-06-07 00:45:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // static menu utama API di /api
-app.use('/api', express.static(path.join(__dirname, 'api-page')));
+app.use('/api-page', express.static(path.join(__dirname, 'api-page')));
 
 // middleware untuk response JSON
 const settingsPath = path.join(__dirname, './src/settings.json');
