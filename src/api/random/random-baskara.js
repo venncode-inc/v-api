4612e5cbe4eddaff051c78e244cd1410d3805795
Hz4E!_ <mrayhan1924@gmail.com>
2025-06-20 19:10:35 +0700

    random-baskara.js

---
+++ src/api/random/random-baskara.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+module.exports = function(app) {
+    async function getRandomBaskara() {
+        try {
+            const { data } = await axios.get('https://raw.githubusercontent.com/hazelnuttty/API/main/baskara.json');
+            const list = Array.isArray(data) ? data : data.quotes || [];
+            const randomText = list[Math.floor(Math.random() * list.length)];
+            return randomText;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    app.get('/random/kataislami', async (req, res) => {
+        try {
+            const result = await getRandomBaskara();
+            res.json({
+                status: true,
+                creator: 'Hazel',
+                message: result
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: false,
+                message: `Error: ${error.message}`
+            });
+        }
+    });
+};
